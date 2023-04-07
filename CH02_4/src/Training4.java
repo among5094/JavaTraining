73df3c9aecb3d6a21f726bca650769f22dbe0aa5
among5094 <among5094@gmail.com>
2023-04-07 16:22:24 +0900

    ch02강

--- CH02_4/src/Training4.java
+++ CH02_4/src/Training4.java
@@ -1,4 +1,21 @@
+import java.util.Scanner;
 
 public class Training4 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in); //스캐너 객체 생성
+		System.out.println("점 (x,y)의 좌표를 입력하세요>> ");
+		
+		int x=sc.nextInt();
+		int y=sc.nextInt();
+		
+		if(x>=50 && x<=100 && y>=50 && y<=100 )
+			System.out.println("점("+x+","+y+")"+"은 (50,50)과 (100,100)의 사각형 내에 있습니다.");
+		else 
+			System.out.println("점("+x+","+y+")"+"은 (50,50)과 (100,100)의 사각형 내에 없습니다.");
+		
+		sc.close();
+	}
 
 }
