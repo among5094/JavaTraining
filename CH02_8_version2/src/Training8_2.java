c5dec32d07b94449beeba1adbd449e8fe3f7f399
among5094 <among5094@gmail.com>
2023-04-08 21:25:37 +0900

    ch02

---
+++ CH02_8_version2/src/Training8_2.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Training8_2 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.print("커피를 주문하세요>>");
+		
+		String coffee = sc.next();
+		int Quantity = sc.nextInt();
+		
+		switch(coffee){
+		
+		case "에스프레소":
+			System.out.println(2000*Quantity+"원입니다.");
+			break;
+			
+		case "아메리카노":
+			System.out.println(2500*Quantity+"원입니다.");
+			break;
+			
+		case "카푸치노":
+			System.out.println(3000*Quantity+"원입니다.");
+			break;
+			
+		case "카페라떼":
+			System.out.println(3500*Quantity+"원입니다.");
+			break;
+		
+		}
+		
+		
+		sc.close();
+	}
+
+}
